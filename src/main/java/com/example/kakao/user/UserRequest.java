2cad949c9dcf1699a127a899edd3c33b1f9ec1fe
MMGGHD <tldmseogus@naver.com>
2023-09-30 02:50:47 +0900

    [김대현] AOP 유효성검사

--- src/main/java/com/example/kakao/user/UserRequest.java
+++ src/main/java/com/example/kakao/user/UserRequest.java
@@ -1,17 +1,24 @@
 package com.example.kakao.user;
 
+import javax.validation.constraints.NotEmpty;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
 
 public class UserRequest {
     @Getter
     @Setter
     @ToString
     public static class JoinDTO {
+
+        @NotEmpty(message = "email은 비어있을수 없습니다.")
         private String email;
+
+        @NotEmpty(message = "password는 비어있을수 없습니다.")
         private String password;
+
+        @NotEmpty(message = "username은 비어있을수 없습니다.")
         private String username;
 
         public User toEntity() {
@@ -27,7 +34,11 @@ public class UserRequest {
     @Setter
     @ToString
     public static class LoginDTO {
+
+        @NotEmpty(message = "email을 입력해주세요.")
         private String email;
+
+        @NotEmpty(message = "password를 입력해주세요.")
         private String password;
     }
 }
