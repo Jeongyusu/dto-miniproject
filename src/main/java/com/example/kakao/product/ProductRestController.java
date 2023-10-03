d0965b6245eb460ea9dc0b3f64e0faac421cf8c7
Jeongyusu <130957655+Jeongyusu@users.noreply.github.com>
2023-10-03 20:49:40 +0900

    코드 드 ìµ코드 ì최종 프로젝트 영상 세팅 완료

--- src/main/java/com/example/kakao/product/ProductRestController.java
+++ src/main/java/com/example/kakao/product/ProductRestController.java
@@ -1,5 +1,10 @@
 package com.example.kakao.product;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.kakao._core.utils.ApiUtils;
+import com.example.kakao.product.option.Option;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,41 +20,49 @@ import lombok.RequiredArgsConstructor;
 @RestController
 public class ProductRestController {
 
-    private final ProductService productService;
+    private final ProductService productService; // 자바에서 final 변수는 반드시 초기화되어야 함.
+    private final HttpSession session;
 
     // (기능1) 상품 목록보기
     @GetMapping("/products")
     public ResponseEntity<?> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page) {
-        return ResponseEntity.ok().body(ApiUtils.success(null));
+        List<ProductResponse.FindAllDTO> findAllDTOs = productService.findAll(page);
+        return ResponseEntity.ok().body(ApiUtils.success(findAllDTOs));
     }
 
     // (기능2) 상품 상세보기
     @GetMapping("/products/{id}")
     public ResponseEntity<?> findById(@PathVariable int id) {
+        ProductResponse.FindByIdV2DTO responseDTO = productService.findByIdV2(id);
+        return ResponseEntity.ok(responseDTO);
 
-        return null;
     }
 
-
-
-    // -----------------------------
-
-        
-        @GetMapping("/products/{id}/v1")
-    public ResponseEntity<?> findByIdv1(@PathVariable int id) {
-        
-        return null;
+    // 상품조회 + 옵션조회
+    @GetMapping("/products/{id}/v1")
+    public ResponseEntity<?> findByIdV1(@PathVariable int id) {
+        ProductResponse.FindByIdV1DTO responseDTO = productService.findByIdV1(id);
+        return ResponseEntity.ok(responseDTO);
     }
 
-        @GetMapping("/products/{id}/v2")
-    public ResponseEntity<?> findByIdv2(@PathVariable int id) {
-        
-        return null;
+    // 상품조회 양방향 매핑
+    @GetMapping("/products/{id}/v2")
+    public ResponseEntity<?> findByIdV2(@PathVariable int id) {
+        ProductResponse.FindByIdV2DTO responseDTO = productService.findByIdV2(id);
+        return ResponseEntity.ok(responseDTO);
     }
 
-        @GetMapping("/products/{id}/v3")
-    public ResponseEntity<?> findByIdv3(@PathVariable int id) {
-        
-        return null;
+    // 옵션조회
+    @GetMapping("/products/{id}/v3")
+    public ResponseEntity<?> findByIdV3(@PathVariable int id) {
+        ProductResponse.FindByIdV3DTO responseDTO = productService.findByIdV3(id);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    // 옵션조회
+    @GetMapping("/products/{id}/v4")
+    public ResponseEntity<?> findByIdV4(@PathVariable int id) {
+        List<Option> responseDTO = productService.findByIdV4(id);
+        return ResponseEntity.ok(responseDTO);
     }
 }
